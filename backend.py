4b6fe64a2dfa77bee80a82ab26507c686814d0d3
Repale Prashant Gangaram <prashantgrepale2002@gmail.com>
2026-06-16 11:28:24 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -115,14 +115,14 @@ def rag_tool(query: str) -> dict:
     """
     
     results = retriever.invoke(query)
-    relevance_check = db.similarity_search_with_relevance_scores(query, k=1)
+    # relevance_check = db.similarity_search_with_relevance_scores(query, k=1)
 
-    if not relevance_check or relevance_check[0][1] < 0.1:
-        return {
-            # "status": 
-            "instruction": "If no relevant documents are found, use finance_news_search tool only for finance-related queries; otherwise, do not use any tools and politely respond that only finance-related questions are supported.",
-            "context": [],
-        }
+    # if not relevance_check or relevance_check[0][1] < 0.1:
+    #     return {
+    #         # "status": 
+    #         "instruction": "If no relevant documents are found, use finance_news_search tool only for finance-related queries; otherwise, do not use any tools and politely respond that only finance-related questions are supported.",
+    #         "context": [],
+    #     }
     context = [doc.page_content for doc in results]
     metadata = [doc.metadata for doc in results]
     return {"query": query, "context": "\n\n".join(context), "metadata": metadata}
